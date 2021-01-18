53e5000e367126c02c9aa2a8e8b090693d266335
Mobarak Ali <mobarak.ali@outlook.com>
2021-01-18 22:36:21 +0600

    lesson 02 completed

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ https://mobarak-ali.github.io/module14/
 ## 14-1 Module Introduction Apply JS
 
 ## 14-2 Unit Convert Inch to Feet use variable and function
+https://mobarak-ali.github.io/module14/lesson02.html
---
+++ lesson02.js
@@ -0,0 +1,19 @@
+// var inch = 165;
+// var feet  = inch / 12;
+function inchToFeet(inch){
+    var feet = inch/12;
+    return feet;
+}
+var nanaFeet = inchToFeet(156);
+console.log(nanaFeet);
+
+var naniFeet = inchToFeet(288);
+console.log(naniFeet);
+
+var dadiiFeet = inchToFeet(300);
+console.log(dadiiFeet);
+
+var senior = [250,155,168];
+var dadaFeet = inchToFeet(senior[0]);
+
+console.log(dadaFeet);
